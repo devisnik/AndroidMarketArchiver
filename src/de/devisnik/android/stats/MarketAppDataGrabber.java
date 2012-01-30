6f1641c021210161e8ff2a54edbf331df8617d62
Volker Leck <volker.leck@gmail.com>
2012-01-30 13:42:31 +0100

    updated wait for ajax elements

--- src/de/devisnik/android/stats/MarketAppDataGrabber.java
+++ src/de/devisnik/android/stats/MarketAppDataGrabber.java
@@ -5,10 +5,14 @@ import java.io.UnsupportedEncodingException;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverBackedSelenium;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
@@ -27,9 +31,11 @@ public class MarketAppDataGrabber {
 		private String mPassword;		
 	}
 
+	private static final boolean verbose = true;
 	private static final String VALID_ROWS_XPATH = "//div[@class='listingTable']/child::div[@style='']/div[@class='listingRow']";
 	private static DefaultSelenium selenium;
-
+	
+	
 	public static void main(String[] args) throws Exception {
 		LogManager.getLogManager().reset();
 		Logger globalLogger = Logger.getLogger(java.util.logging.Logger.GLOBAL_LOGGER_NAME);
@@ -52,6 +58,7 @@ public class MarketAppDataGrabber {
 	}
 
 	private Params mParams;
+	private static HtmlUnitDriver mDriver;
 
 	public MarketAppDataGrabber(Params params) {
 		mParams = params;
@@ -62,19 +69,28 @@ public class MarketAppDataGrabber {
 	}
 
 	private static void startServerAndClient() throws Exception {
-		HtmlUnitDriver driver = new HtmlUnitDriver(DesiredCapabilities.firefox());
-		driver.setJavascriptEnabled(true);
-		selenium = new WebDriverBackedSelenium(driver, "http://market.android.com");
+		mDriver = new HtmlUnitDriver(DesiredCapabilities.firefox());
+		mDriver.setJavascriptEnabled(true);
+		selenium = new WebDriverBackedSelenium(mDriver, "http://market.android.com");
 	}
 
 	private void grab() throws InterruptedException, IOException {
 		signIn();
-		waitForElementPresent(VALID_ROWS_XPATH, 30000);
+		Thread.sleep(2000);
+		waitForAppTableLoaded();
 		// extra wait to ensure all rows are present
 		Thread.sleep(2000);
 		Number count = selenium.getXpathCount(VALID_ROWS_XPATH);
 		for (int index = 1; index <= count.intValue(); index++)
 			printoutRatings(VALID_ROWS_XPATH + "/../div[" + index + "]");
+	}
+
+	private void waitForAppTableLoaded() throws InterruptedException {
+		if (verbose)
+			System.out.println("start waiting for app data loaded");
+		waitForElementPresent(VALID_ROWS_XPATH, 30);
+		if (verbose)
+			System.out.println("app data fully loaded");
 	}
 
 	/**
@@ -83,17 +99,23 @@ public class MarketAppDataGrabber {
 	 * @throws UnsupportedEncodingException
 	 */
 	private void signIn() throws UnsupportedEncodingException {
+		if (verbose)
+			System.out.println("signing in!");
 		selenium.open("https://www.google.com/accounts/ServiceLogin?service=androiddeveloper&passive=true&nui=1&continue=http://market.android.com/publish/Home");
 		selenium.type("Email", mParams.mLogin);
 		selenium.type("Passwd", mParams.mPassword);
 		selenium.click("signIn");
 		selenium.waitForPageToLoad("120000");
+		if (verbose)
+			System.out.println("done signing in!");
 	}
 
 	/**
 	 * printoutRatings.
 	 */
 	private void printoutRatings(String rootTag) throws InterruptedException, IOException {
+		if (verbose)
+			System.out.println("opening /publish/Home");
 		selenium.open("/publish/Home");
 
 		String commentsLink = rootTag + "/table/tbody/tr[2]/td/div/a";
@@ -120,6 +142,8 @@ public class MarketAppDataGrabber {
 	}
 
 	private void readBasicAppData(String rootTag, AppData appData) {
+		if (verbose)
+			System.out.println("reading "+rootTag);
 		String downloadsTag = rootTag + "/div[2]/div[1]/span";
 		String activeInstallsTag = rootTag + "/div[2]/div[2]/span[1]";
 		String nameTag = rootTag + "/div[1]/a";
@@ -128,18 +152,33 @@ public class MarketAppDataGrabber {
 		appData.setProperty(Property.VERSION, selenium.getText(versionTag));
 		appData.setProperty(Property.DOWNLOADS, selenium.getText(downloadsTag));
 		appData.setProperty(Property.INSTALLS, selenium.getText(activeInstallsTag));
+		if (verbose)
+			System.out.println("done reading "+rootTag);		
 	}
 
 	private void waitForElementPresent(String elementXPath, int maxWaitInSeconds) throws InterruptedException {
-		for (int second = 0;; second++) {
-			if (second >= maxWaitInSeconds)
-				throw new RuntimeException("timeout");
-			try {
-				if (selenium.isElementPresent(elementXPath))
-					break;
-			} catch (Exception e) {
-			}
-			Thread.sleep(1000);
-		}
+		WebDriverWait wait = new WebDriverWait(mDriver, maxWaitInSeconds);
+		wait.until(visibilityOfElementLocated(By.xpath(elementXPath)));
+
+//		for (int second = 0;; second++) {
+//			if (second >= maxWaitInSeconds)
+//				throw new RuntimeException("timeout");
+//			try {
+//				if (selenium.isElementPresent(elementXPath))
+//					break;
+//			} catch (Exception e) {
+//			}
+//			Thread.sleep(1000);
+//		}
 	}
+	
+    public ExpectedCondition<WebElement> visibilityOfElementLocated(final By by) {
+        return new ExpectedCondition<WebElement>() {
+          public WebElement apply(WebDriver driver) {
+            WebElement element = driver.findElement(by);
+            return element.isDisplayed() ? element : null;
+          }
+        };
+      }
+
 }
