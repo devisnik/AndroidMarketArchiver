863d74b6795df1fb925a04c1d94a95c5f51c770c
Volker Leck <volker.leck@gmail.com>
2012-03-19 16:17:24 +0100

    updated grab shell script to use selenium 2.20.0

--- grab.sh
+++ grab.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java -cp lib/selenium-2.15.0/*:lib/selenium-2.15.0/libs/*:build/marketGrabber.jar de.devisnik.android.stats.MarketAppDataGrabber -login $1 -pass $2
+java -cp lib/selenium-2.20.0/*:lib/selenium-2.20.0/libs/*:build/marketGrabber.jar de.devisnik.android.stats.MarketAppDataGrabber -login $1 -pass $2
 
